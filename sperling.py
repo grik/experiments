be1e40ab4567b72476513db485635931fc25f8a7
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2016-05-04 13:40:31 +0200

    matices generator and dialogbox ready

--- sperling.py
+++ sperling.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 
 '''
 name:
@@ -12,9 +13,10 @@ This script allows to conduct a cognitive experiment which is a replication
 of an experiment originaly designed by George Sperling.
 '''
 
-from psychopy import visual, event, core, sound
-from random import randint
+from psychopy import visual, event, core, sound, gui
+import random
 import datetime
+import csv
 
 
 ################################################################
@@ -53,21 +55,25 @@ quit_experiment = False
 # ### STIMULI SETTINGS
 
 # size of the font for stimuli
-fontsize = 70
+fontsize_stimuli = 70
+fontsize_instruction = 30
 
-instruction_text = 'You will be shown boards with letters. '
-instruction_text += 'After that you will hear one of three tones: '
-instruction_text += 'high, medium or low. \n'
-instruction_text += 'Your task is ... \n\n'
-instruction_text += 'Press space if you are ready'
+instruction_text = u'Za chwilę zostaną Ci zaprezentowane tablice ze znakami. '
+instruction_text += u'Każda tablica będzie miała trzy rzędy. '
+instruction_text += u'Po prezentacji każdej tablicy usłyszysz jeden z '
+instruction_text += u'trzech dźwięków: wysoki, średni lub niski. \n'
+instruction_text += u'Twoim zadaniem będzie przypomnienie sobie oraz zapisanie '
+instruction_text += u'znaków z rzędu wskazanego przez wysokość dźwięku: '
+instruction_text += u'odpowiednio: najwyższego rzędu, średniego lub dolnego. \n'
+instruction_text += u'\nAby rozpocząć naciśnij spację. '
 
 # with mask or without after the stimulus
 mask_appear = True
 
 # how many trials in one experiment
-n_trials = 10
+n_trials = 2
 
-# matrix size
+# matrix size: x rows, y columns
 x, y = 3, 4
 
 ########################
@@ -79,8 +85,11 @@ x, y = 3, 4
 # # Polish upper cases (without diacritics)
 # signs = 'ABCDEFGHIJKLMNOPRSTUWZ'
 
-# English upper cases
-signs = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# # English upper cases
+# signs = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# English upper cases (consonants only, without 'Y')
+signs = 'BCDFGHJKLMNPQRSTVWXZ'
 ########################
 
 # time before first stimulus appears, after instruction (in seconds)
@@ -133,7 +142,7 @@ iti = 1.0
 
     Na koniec musi sie tez wygenerowac maska o takich samych rozmiarach
     jak matryca.
-'''
+
 stimuli_raw = [
     [
         'A B C D',
@@ -153,10 +162,8 @@ mask_raw = [
     '# # # #'
     ]
 
-'''
     Od tego miejsca program sam przemieli zmienna 'stimuli_raw' tak, zeby dalo
     sie ja wyswietlic za pomoca psychopy.
-'''
 
 stimuli = []
 mask = ''.join([i + '\n' for i in mask_raw])
@@ -165,6 +172,38 @@ for stimulus_raw in stimuli_raw:
     stimuli.append(''.join([i + '\n' for i in stimulus_raw]))
 
 trials = []
+'''
+
+
+signs_list = []
+for char in signs:
+    signs_list.append(char)
+
+mask = ''
+for i in range(x):
+        for j in range(y):
+            mask += '# '
+        mask += '\n'
+
+
+def generateStimulus(row, signs_list, x, y):
+    random.shuffle(signs_list)
+    stimulus = ''
+    stimulus_list = []
+    n = 0
+    for i in range(x):
+        verse = ''
+        for j in range(y):
+            verse += signs_list[j+n] + ' '
+        n += y
+        stimulus_list.append(verse)
+    correct = ''.join(stimulus_list[row])
+    correct = correct.replace(' ', '')
+    stimulus = ''.join(i + '\n' for i in stimulus_list)
+    return stimulus, correct
+
+trials = []
+
 # ### END OF GENERATE STIMULI
 ################################
 
@@ -176,19 +215,19 @@ instruction = visual.TextStim(
     win=mywin,
     text=instruction_text,
     font='Monospace',
-    pos=(0, 0), height=fontsize, wrapWidth=win_width*0.9
+    pos=(0, 0), height=fontsize_instruction, wrapWidth=win_width*0.9
     )
 
 mask = visual.TextStim(
     win=mywin,
     text=mask, font='Monospace',
-    pos=(0, 0), height=fontsize
+    pos=(0, 0), height=fontsize_stimuli
     )
 
 empty = visual.TextStim(
     win=mywin,
     text='', font='Monospace',
-    pos=(0, 0), height=fontsize
+    pos=(0, 0), height=fontsize_stimuli
     )
 # ### END OF CONSTANT BOARDS
 ################################
@@ -241,16 +280,13 @@ while 'space' not in event.getKeys():
     pass
 
 # iterate across all stimuli
-for i in range(len(stimuli)):
-
-    # set current stimulus
-    stimulus = stimuli[i]
+for i in range(n_trials):
 
     # randomly choose which row will be recalled
-    row = randint(0, 2)
+    row = random.randint(0, 2)
 
-    # the correct answer is...
-    correct = stimuli_raw[i][row].replace(' ', '')
+    # set current stimulus
+    stimulus, correct = generateStimulus(row, signs_list, x, y)
 
     print(correct)
 
@@ -258,7 +294,7 @@ for i in range(len(stimuli)):
     matrix = visual.TextStim(
         win=mywin,
         text=stimulus, font='Monospace',
-        pos=(0, 0), height=fontsize
+        pos=(0, 0), height=fontsize_stimuli
         )
 
     # diplay the matrix of signs
@@ -295,7 +331,7 @@ for i in range(len(stimuli)):
     visual.TextStim(
         win=mywin,
         text=''.join(text_typed), font='Monospace',
-        pos=(0, -100), height=fontsize
+        pos=(0, -100), height=fontsize_stimuli
         ).draw()
     mywin.flip()
 
@@ -346,19 +382,38 @@ for i in range(len(stimuli)):
 
     trials.append(int(correct == answer))
 
-trials_correct = sum(trials)
-trials_total = len(trials)
+if not trials:
+    'Quitting, not even one trial acquired'
+else:
+    trials_correct = sum(trials)
+    trials_total = len(trials)
 
-percent_correct = int((sum(trials)/float(len(trials))) * 100)
-print('correct answers: %s/%s' % (sum(trials), len(trials)))
-print('percent correct: %s%%' % percent_correct)
+    percent_correct = int((sum(trials)/float(len(trials))) * 100)
+    print('correct answers: %s/%s' % (sum(trials), len(trials)))
+    print('percent correct: %s%%' % percent_correct)
 
-with open('output_%s.txt' % time_code, 'w') as text_file:
-    text_file.write(
-        '{}, {}, {}\n'.format(trials_correct, trials_total, percent_correct)
-        )
+    scores = [trials_correct, trials_total, percent_correct]
+
+    info = {
+        'kierunek(p=psycho/c=cogni/i=inny/n=nie studiuje)': 'i',
+        'wiek': 21,
+        'plec(k/m)': 'k'
+        }
+    box = gui.DlgFromDict(dictionary=info, title='Dane')
+    if not (box.OK):
+        core.quit()
+
+    register = []
+    register.append(info['plec(k/m)'])
+    register.append(info['wiek'])
+    register.append(info['kierunek(p=psycho/c=cogni/i=inny/n=nie studiuje)'])
+
+    with open('output/output_%s.csv' % time_code, 'w') as text_file:
+        save = csv.writer(text_file)
+        save.writerow(scores + register)
 
 mywin.close()
+
 
 ################################################################
 #
